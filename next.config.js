d4706b12db7b57198de40796c4d9b746259f395c
paigesheffren <98558640+paigesheffren@users.noreply.github.com>
2025-10-31 13:36:10 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: process.env.BASE_PATH || '',
-  assetPrefix: process.env.ASSET_PREFIX || undefined,
-};
-module.exports = nextConfig;
